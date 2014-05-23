e993aba22481b317669fb71a044b6d09761738c2
Raphael Diethelm <r.diethelm@greenmail.ch>
2014-05-23 10:26:55 +0200

    new script

--- javascript/script.js
+++ javascript/script.js
@@ -7,12 +7,12 @@ var options = {
 function success(pos) {
   var crd = pos.coords;
 
-  $("js-long").text(crd.longitude)
-  $("js-lat").text(crd.latitude)
-  $("js-acc").text(crd.accuracy)
+  $('.js-long').text(crd.longitude)
+  $('.js-lat').text(crd.latitude)
+  $('.js-acc').text(crd.accuracy + 'm');
 
   
-};
+}
 
 function error(err) {
   console.warn('ERROR(' + err.code + '): ' + err.message);
