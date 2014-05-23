5a930a09dfe5ad02f4fac8279136e1bf1024239e
Raphael Diethelm <r.diethelm@greenmail.ch>
2014-05-23 11:34:00 +0200

    geocode added

--- javascript/script.js
+++ javascript/script.js
@@ -12,9 +12,13 @@ function success(pos) {
   $('.js-acc').text('+/- ' + crd.accuracy + ' meters');
 
   $.ajax({
-  	url: 'https://api.forecast.io/forecast/b1fe5cae982490b8e60dac5cb2368ad8/' + crd.latitude + ',' + crd.longitude,
+  	url: 'https://maps.googleapis.com/maps/api/geocode/output',
+
+  	//api.forecast.io/forecast/b1fe5cae982490b8e60dac5cb2368ad8/' + crd.latitude + ',' + crd.longitude,
   	data: {
-  		units: 'si'
+  		//units: 'si'
+  		latlng: crd.latitude + ',' + crd.longitude,
+  		sensor: true
   	},
   	dataType: 'jsonp',
   	success: function(data){
@@ -24,14 +28,6 @@ function success(pos) {
   });
 };
 
-function geoFindMe() {
-  var output = document.getElementById("out");
-
-var img = new Image();
-    img.src = "http://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
-
-    output.appendChild(img);
-  };
 
 function error(err) {
   console.warn('ERROR(' + err.code + '): ' + err.message);
