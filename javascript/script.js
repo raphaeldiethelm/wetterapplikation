9eb2f528e5a73218475d1f651c27bd9cc7650d1e
Raphael Diethelm <r.diethelm@greenmail.ch>
2014-06-06 10:28:23 +0200

    add temp

--- javascript/script.js
+++ javascript/script.js
@@ -64,7 +64,7 @@ $('.js-custom-address').on('click', 'a', function(event) {
       success: function(data){
         console.log(data);
         $('.js-custom-address-result').text(data.results[0].geometry.location.lat + ',' + data.results[0].geometry.location.lng);
-        $('.js-temp').text(data.currently.apparentTemperature + ' °C');
+        $('.js-temp').text(data.results[0].geometry.location.currently.apparentTemperature + ' °C');
       }
   });
 
