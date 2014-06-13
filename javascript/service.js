d976cad67967ef142fbb65fad4ec55d4d790a6b9
Raphael Diethelm <r.diethelm@greenmail.ch>
2014-06-13 11:43:31 +0200

    a

--- javascript/service.js
+++ javascript/service.js
@@ -1,10 +1,16 @@
-var options = {
-  enableHighAccuracy: true,
-  timeout: 10000,
-  maximumAge: 0
-};
-
 $(document).ready(function() {
+	var options = {
+	  enableHighAccuracy: true,
+	  timeout: 10000,
+	  maximumAge: 0
+	};
+
+	var success = function() {
+		var crd = pos.coords;
+	
+		$('.js-current-position').text(crd.latitude + ', ' + crd.longitude);
+	};
+
 	navigator.geolocation.getCurrentPosition(success, error, options);
 });
 
